ef8d06dfcd1c2d76c100b5515a4a65187d4a9ef1
Krishnanunni V <krishnavijay733@gmail.com>
2021-04-22 22:35:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 grid = []
 for i in range(11):
-    for i in range (11):
+    for j in range (11):
         grid.append(rg.Point3d(i,j,0))
  
 a = grid
+b= rg.Line(grid[4],grid[16])
